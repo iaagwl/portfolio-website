d84125fd615368950d7558931a1508d74c78cb3a
iaagwl <iaagwl@gmail.com>
2017-06-06 00:46:58 +0200

    fix scroll navigation

--- js/menu.js
+++ js/menu.js
@@ -4,87 +4,49 @@ $(document).ready(function(){
         $("nav ul").toggleClass("open");
         $(this).toggleClass("open");
     });
-    //Viewport Width
-    let viewPortWidth = $( window ).width();
-
-    //Changes active class on changing viewports
-    $(window).resize(function() {
-      viewPortWidth = $( window ).width();
-      console.log(viewPortWidth);
-      activeNavigation(viewPortWidth);
-    });
-
     //Initializing active class
-    //$("nav ul li a.home").addClass('active');
-    activeNavigation(viewPortWidth);
-
+    activeNavigation();
 });
 
-function activeNavigation(viewPortWidth) {
-  const home = $("nav ul li a.home");
-  const projects = $("nav ul li a.projects");
-  const about = $("nav ul li a.about");
-  const contact = $("nav ul li a.contact");
-  const homePos =  $("#home").offset().top -500;
-  const projectsPos = $("#projects").offset().top - 500;
-  const aboutPos = $("#about").offset().top - 500;
-  const contactPos = $("#contact").offset().top - 500;
+function activeNavigation() {
+  const home = $("nav ul li a.home"),
+        projects = $("nav ul li a.projects"),
+        about = $("nav ul li a.about"),
+        contact = $("nav ul li a.contact"),
+        homePos =  $("#home").offset().top -500,
+        projectsPos = $("#projects").offset().top - 500,
+        aboutPos = $("#about").offset().top - 500,
+        contactPos = $("#contact").offset().top - 500;
   let last_known_scroll_position = 0;
 
   last_known_scroll_position = window.scrollY;
   if (last_known_scroll_position > contactPos){
-    home.removeClass("active");
-    projects.removeClass("active");
-    about.removeClass("active");
-
+    home.add(projects).add(about).removeClass("active");
     contact.addClass("active");
-
   } else if (last_known_scroll_position > aboutPos){
-    home.removeClass("active");
-    projects.removeClass("active");
-    contact.removeClass("active");
-
+    home.add(projects).add(contact).removeClass("active");
     about.addClass("active");
   } else if (last_known_scroll_position > projectsPos){
-    home.removeClass("active");
-    about.removeClass("active");
-    contact.removeClass("active");
-
+    home.add(contact).add(about).removeClass("active");
     projects.addClass("active");
   } else {
-    about.removeClass("active");
-    projects.removeClass("active");
-    contact.removeClass("active");
-
+    about.add(projects).add(contact).removeClass("active");
     home.addClass("active");
   }
 
   $( window ).scroll(function() {
     last_known_scroll_position = window.scrollY;
     if (last_known_scroll_position > contactPos){
-      home.removeClass("active");
-      projects.removeClass("active");
-      about.removeClass("active");
-
+      home.add(projects).add(about).removeClass("active");
       contact.addClass("active");
-
     } else if (last_known_scroll_position > aboutPos){
-      home.removeClass("active");
-      projects.removeClass("active");
-      contact.removeClass("active");
-
+      home.add(projects).add(contact).removeClass("active");
       about.addClass("active");
     } else if (last_known_scroll_position > projectsPos){
-      home.removeClass("active");
-      about.removeClass("active");
-      contact.removeClass("active");
-
+      home.add(contact).add(about).removeClass("active");
       projects.addClass("active");
     } else {
-      about.removeClass("active");
-      projects.removeClass("active");
-      contact.removeClass("active");
-
+      about.add(projects).add(contact).removeClass("active");
       home.addClass("active");
     }
   });
